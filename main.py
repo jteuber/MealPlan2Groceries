a92619f7654ec10047e63dbd3ceaf4b4fc3e06e2
Jörn Teuber <jteuber@jaytea.org>
2023-06-13 00:26:11 +0200

    First practically working version

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 
 from tabulate import tabulate
 import csv
+import sys
 
 
 def parse_meal_plan(meal_plan_file_path):
     # Read the meal plan from the file
-    with open('meal_plan.md', 'r') as meal_plan_file:
+    with open(meal_plan_file_path, 'r') as meal_plan_file:
         meal_plan_table = meal_plan_file.read()
 
     # Parse the meal plan table and extract the recipe names
-    meal_plan = tabulate([row.split("|")[1:-1] for row in meal_plan_table.split("\n")
-                          if "|" in row], headers="firstrow", tablefmt="pipe")
+    meal_plan = [row.split("|")[1:-1]
+                 for row in meal_plan_table.split("\n") if "|" in row]
+
+    meal_plan = meal_plan[-7:]
+
+    for i in range(7):
+        meal_plan[i][0] = meal_plan[i][0].strip().lower()
+        meal_plan[i][1] = meal_plan[i][1].strip().lower()
 
     return meal_plan
 
@@ -19,15 +26,18 @@ def parse_meal_plan(meal_plan_file_path):
 def parse_ingredient_file(ingredients_file_path):
     ingredient_info = {}
 
-    with open(ingredients_file_path, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            ingredient_name = row['Ingredient Name']
-            ingredient_info[ingredient_name] = {
-                'Dutch Name': row['Dutch Name'],
-                'Supermarket Link': row['Supermarket Link'],
-                'Available at Farmers Market': row['Available at Farmers Market']
-            }
+    try:
+        with open(ingredients_file_path, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                ingredient_name = row['Ingredient Name'].strip().lower()
+                ingredient_info[ingredient_name] = {
+                    'Dutch Name': row['Dutch Name'],
+                    'Supermarket Link': row['Supermarket Link'],
+                    'Available at Farmers Market': row['Available at Farmers Market']
+                }
+    except FileNotFoundError:
+        pass
 
     return ingredient_info
 
@@ -43,11 +53,10 @@ def parse_recipe_file(recipe_file_path):
         line = line.strip()
 
         if line.startswith('## '):
-            recipe_name = line[3:]
-            current_recipe = recipe_name
+            current_recipe = line[3:].strip().lower()
             recipes[current_recipe] = []
         elif current_recipe and line.startswith('* [ ] '):
-            ingredient = line[7:]
+            ingredient = line[6:].strip().lower()
             recipes[current_recipe].append(ingredient)
 
     return recipes
@@ -62,20 +71,35 @@ def assemble_grocery_list(meal_plan, recipes):
         # Assuming the recipe name is in the first column
         recipe_name = meal[1]
 
+        if '+' in recipe_name:
+            compound_recipe = recipe_name.split('+')
+            for sub_recipe in compound_recipe[1:]:
+                meal_plan.append(['sub', sub_recipe.strip()])
+            recipe_name = compound_recipe[0].strip()
+
         if recipe_name in recipes:
             for ingredient in recipes[recipe_name]:
-                # Split the line into quantity and ingredient name
-                ingredient_parts = ingredient.split(' ', 1)
-                quantity = ingredient_parts[0]
-                ingredient_name = ingredient_parts[1]
+                ingredient_name = ingredient
+                quantity = 1
+                unit = ''
+
+                if ingredient[0].isnumeric():
+                    # Split the line into quantity and ingredient name
+                    ingredient_parts = ingredient.split(' ', 1)
+                    unit = ingredient_parts[0].lstrip('0123456789.')
+                    quantity = float(
+                        ingredient_parts[0][0:len(ingredient_parts[0])-len(unit)])
+                    ingredient_name = ingredient_parts[1]
 
                 # Check if the ingredient is already in the grocery list
                 if ingredient_name in grocery_list:
                     # If it exists, add the quantity to the existing quantity
-                    grocery_list[ingredient_name] += quantity
+                    grocery_list[ingredient_name][0] += quantity
                 else:
                     # If it's a new ingredient, add it to the grocery list
-                    grocery_list[ingredient_name] = quantity
+                    grocery_list[ingredient_name] = [quantity, unit]
+        else:
+            print(f"recipe \"{recipe_name}\" not found in the recipe list")
 
     return grocery_list
 
@@ -86,7 +110,8 @@ def enhance_grocery_list(grocery_list, ingredient_info):
         if item in ingredient_info:
             groceries.append({
                 'name': item,
-                'quantity': grocery_list[item],
+                'quantity': grocery_list[item][0],
+                'unit': grocery_list[item][1],
                 'Dutch Name': ingredient_info[item]['Dutch Name'],
                 'Supermarket Link': ingredient_info[item]['Supermarket Link'],
                 'Available at Farmers Market': ingredient_info[item]['Available at Farmers Market']
@@ -94,17 +119,22 @@ def enhance_grocery_list(grocery_list, ingredient_info):
         else:
             groceries.append({
                 'name': item,
-                'quantity': grocery_list[item]
+                'quantity': grocery_list[item][0],
+                'unit': grocery_list[item][1]
             })
+    return groceries
 
 
-meal_plan = parse_meal_plan("Meal Plan.txt")
-recipes = parse_recipe_file("Recipes.txt")
+path = sys.argv[1]
+
+meal_plan = parse_meal_plan(path + "/Meal Plan.txt")
+recipes = parse_recipe_file(path + "/Recipes.txt")
 grocery_list = assemble_grocery_list(meal_plan, recipes)
 
-ingredient_info = parse_ingredient_file("ingredients.csv")
-
+ingredient_info = parse_ingredient_file(path + "/ingredients.csv")
+groceries = enhance_grocery_list(grocery_list, ingredient_info)
 
 # Print the combined grocery list
-for ingredient, quantity in grocery_list:
-    print(f"{ingredient}: {quantity}")
+for ingredient in groceries:
+    print(
+        f"* [ ] {ingredient['name']}: {ingredient['quantity']}{ingredient['unit']}")
